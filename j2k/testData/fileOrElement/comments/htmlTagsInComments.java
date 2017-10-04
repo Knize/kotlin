2023e4855c0c68dc8a6d045d997f8954b483e4e3
knize <kirill.knize@gmail.com>
2017-10-05 01:45:19 +0300

    KT-13019 test

---
+++ j2k/testData/fileOrElement/comments/htmlTagsInComments.java
@@ -0,0 +1,11 @@
+/**
+ * Doc comment.<p>
+ *
+ * {@code sample inline code}
+ *
+ * <pre>
+ *     Sample pre, line 1.
+ *     Sample pre, line 2.
+ * </pre>
+ */
+public class MyClass {}
---
+++ j2k/testData/fileOrElement/comments/htmlTagsInComments.kt
@@ -0,0 +1,13 @@
+/**
+ * Doc comment.
+ *
+ *
+ *
+ * `sample inline code`
+ *
+ * <pre>
+ * Sample pre, line 1.
+ * Sample pre, line 2.
+ * </pre>
+ */
+class MyClass
