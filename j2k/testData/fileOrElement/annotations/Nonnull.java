a17f2d5a88e89ce04797325c3e9bed052f4280b4
knize <kirill.knize@gmail.com>
2017-10-08 21:32:57 +0300

    KT-20023 test

---
+++ j2k/testData/fileOrElement/annotations/Nonnull.java
@@ -0,0 +1,4 @@
+@Nonnull(when = When.ALWAYS)
+@TypeQualifierDefault(ElementType.TYPE_USE)
+public @interface MaybeTypeuse {
+}
---
+++ j2k/testData/fileOrElement/annotations/Nonnull.kt
@@ -0,0 +1,3 @@
+@Nonnull(when = When.ALWAYS)
+@TypeQualifierDefault(ElementType.TYPE_USE)
+annotation class MaybeTypeuse
